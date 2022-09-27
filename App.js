17a6a3543ee0a994c46b217ff7d400ca03c5ad7c
Zeca Cordeiro <zecaxcr@gmail.com>
2022-09-27 20:41:45 -0300

    create-react-native-app

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles2.container}>Meu primeiro aplicativo! hahaha</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -17,5 +17,13 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+});
+
+const styles2 = StyleSheet.create({
+  container: {
+    backgroundColor: 'red',
+    color: 'white',
+    fontSize: 40,
   },
 });
